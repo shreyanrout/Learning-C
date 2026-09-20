7a398c96d4d3ccff379465cb630e66e28ddd4de2
shreyanrout <shreyanrout@gmail.com>
2026-09-20 23:22:32 +0530

    Third variables code

---
+++ variables3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+double cost = 2300.752;
+double sale = 3000.838;
+
+printf("Cost price of silver: %lf \n",cost);
+printf("Selling price of silver: %lf",sale);
+
+
+    return 0;
+}
