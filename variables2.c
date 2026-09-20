653ccaa911b3d44965c5d2382e140e0b81100635
shreyanrout <shreyanrout@gmail.com>
2026-09-20 22:55:25 +0530

    Second variable code

---
+++ variables2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+
+ float gpa = 9.3;
+ float price = 1000;
+
+ printf("Your gpa is: %f \n",gpa);
+ printf("Your total amount is: %f ",price);
+
+
+
+    return 0;
+}
