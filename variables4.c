e4ccf10d30d9afb46c88cc6453b69e6adfa00d25
shreyanrout <shreyanrout@gmail.com>
2026-09-21 23:16:24 +0530

    fourth and last variables code

--- variables4.c
+++ variables4.c
@@ -1,1 +1,12 @@
-#in
+#include <stdio.h>
+int main (){
+
+char grade = 'F';
+char symbol = '!';
+
+printf("Your grade is %c\n", grade);
+printf("Your favourite symbol is %c", symbol);
+
+
+    return 0;
+}
